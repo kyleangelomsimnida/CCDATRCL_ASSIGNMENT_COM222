5029f61c9e7c6920cb2809341c926569350e4a53
kyleangelomsimnida <142467241+kyleangelomsimnida@users.noreply.github.com>
2023-08-31 16:02:28 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,86 @@
+package ACTIVITY4;
+
+import java.util.Scanner;
+import java.util.jar.Attributes.Name;
+
+import ACTIVITY4.Classpack.Animal;
+import ACTIVITY4.Classpack.Circle;
+import ACTIVITY4.Classpack.Employee;
+import ACTIVITY4.Classpack.Trafficlight;
+
+public class Main {
+/**
+ * @param args
+ */
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    //Tao
+  
+    //  System.out.print("Ilagay ang Pangalan: ");
+    // String pangalan = sc.next();
+
+    // System.out.print("Ilagay ang Edad: ");
+    // int edad = sc.nextInt();
+
+    // Tao objPerson = new Tao(pangalan);
+    // System.out.println("Ikaw si " + pangalan);
+
+    // Tao objAge = new Tao(edad);
+    // System.out.println("Ikaw ay " + edad + " taong gulang");
+
+    // //Animals
+    
+    // System.out.print("Input animal name: ");
+    // String Name = sc.next();
+
+    // System.out.print("Input animal breed: ");
+    // String Breed = sc.next();
+
+    // System.out.print("Input what type of animal");
+    // String Type = sc.next();
+
+    
+    // Animal objanimal = new Animal(Name);
+    // System.out.println("The animal's name is " + Name);
+
+    // Animal objbreed = new Animal(Breed);
+    // System.out.println("The animal's breed is " + Breed);
+
+    // Animal objtype = new Animal(Type);
+    // System.out.println("The animal type is " + Type);
+   
+    //Employee
+    // Employee objEmployee = new Employee();
+
+    // System.out.print("Enter Name of employee: ");
+    // objEmployee.setName(sc.next());
+
+    // System.out.print("Enter Job Title: ");
+    // objEmployee.setJobTitle(sc.next());
+
+    // System.out.print("Enter Number of attendance: ");
+    // objEmployee.setNumOfAttendance(sc.nextDouble());
+
+    // System.out.print("Enter Salary: ");
+    // objEmployee.setSalary(sc.nextDouble());
+
+    // System.out.println();
+    // System.out.println("Your updated salary is: " +objEmployee.getSalary() * objEmployee.getNumOfAttendance());
+
+    //Circle
+
+    
+
+    //Traffic light
+
+    // Trafficlight objtrafficLight = new Trafficlight();
+
+    // System.out.print("Input Color: Red or Green ");
+    // objtrafficLight.setColor(sc.next());
+
+
+    
+}
+
+    
+}
